5c6bc0a7e589751eb6a824d1b65f8847df467ddc
shmakova <shmakova-nastya@yandex.ru>
2016-02-14 21:34:16 +0600

    Initial Commit

---
+++ PhotoFeed/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  PhotoFeed
+//
+//  Created by Mike Spears on 2016-01-07.
+//  Copyright © 2016 YourOganisation. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+
+        return true
+    }
+
+
+    func applicationDidBecomeActive(application: UIApplication) {
+
+    }
+
+
+
+
+}
+
---
+++ PhotoFeed/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  PhotoFeed
+//
+//  Created by Mike Spears on 2016-01-07.
+//  Copyright © 2016 YourOganisation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var updateButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
+    @IBAction func updateButtonAction(sender: AnyObject) {
+
+    }
+    
+}
+
